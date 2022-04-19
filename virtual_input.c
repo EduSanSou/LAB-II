46b7d0db70d9610aa727de52d7148a3ff5549566
EduSanSou <103901211+EduSanSou@users.noreply.github.com>
2022-04-19 00:17:33 -0300

    Update virtual_input.c
    Adicionei alguns #define para definir as funções set_bit e clr_bit, as funções essenciais para cada bloco de código como comentários e uma versão equivalente em C para duas delas (linhas 58 a 66 e 70 a 82). Continuar compreendendo como funcionará a comunicação serial.

--- virtual_input.c
+++ virtual_input.c
@@ -6,6 +6,9 @@
 #define power_pin PD2
 #define control_pin PD3
 #define F_CPU 16000000UL // CPU frequency?
+#define set_bit(Y,bit_x) (Y|=(1<<bit_x))    //ativa o bit x da variável Y (coloca em 1)
+#define clr_bit(Y,bit_x) (Y&=~(1<<bit_x))   //limpa o bit x da variável Y (coloca em 0)
+
 /** Inicialização das variáveis **/
 
 // variável que indica se a FPGA está ligada/desligada.
@@ -35,13 +38,50 @@ void setup(void){
 
 int main() {
   setup();
-  
+  // Loop principal para comunicação e controle da placa DE2-115
   while(1){
-    //
-    // Loop principal para comunicação e controle da placa DE2-115
-    //
+    // Loop cuja condição verifica se há dados para leitura na porta serial
+
+      // Send beacon every 1 second
+
+      // Unblock fpga power in 10 sec after last toggle
+
+      // Disconnect and turn off fpga if user is idle for 10 min
+
+      // Turn fpga off after 10 min of use
+
+    input_millis = clock();
+    //received = Serial.read(); (buscar equivalente)
+
+    // Confirm connection
+    
+    // Disconnect and power off fpga
+    else if (received == 88) {
+      is_connected = false;
+      if (power_state) {
+        is_power_blocked = true;
+        power_off_millis = clock();
+        power_state = 0;
+        clr_bit(PORTD,PD2);
+      }
+    }
+    // Read received byte as bits and set output pins
+
+    // Reset fpga to initial state 
+    else if (received == 127){
+      set_bit(PORTD,PD4);
+      set_bit(PORTD,PD5);
+      set_bit(PORTD,PD6);
+      set_bit(PORTD,PD7);
+      set_bit(PORTB,PB0);
+      set_bit(PORTB,PB1);
+      _delay_ms(1000);       //Delay de 1 segundo
+      set_bit(PORTD,PD3);
+      _delay_ms(1000);       //Delay de 1 segundo
+      clr_bit(PORTD,PD3);
+      }
+    // Toggle fpga power
+    
   }
   return 0;
 }
-
-/**teste**/
