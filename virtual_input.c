67536ca2b9f91783d3bc6ff77118e6dd5723703c
carolinesma <sma.caroline@ymail.com>
2022-04-22 15:38:10 -0300

    Add funções uart

--- virtual_input.c
+++ virtual_input.c
@@ -3,55 +3,101 @@
 #include <avr/io.h> // Header-file that includes the apropriate IO definitions for the device      
 #include <util/delay.h> // Header-file for busy-wait delay loops
 
+#define F_CPU 16000000UL // Frequência do oscilador do arduino 16 MHz
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
 #define power_pin PD2
 #define control_pin PD3
-#define F_CPU 16000000UL // CPU frequency?
 #define set_bit(Y,bit_x) (Y|=(1<<bit_x))    //ativa o bit x da variável Y (coloca em 1)
 #define clr_bit(Y,bit_x) (Y&=~(1<<bit_x))   //limpa o bit x da variável Y (coloca em 0)
 
-/** Inicialização das variáveis **/
+/**Cabeçalho das funções**/
 
-// variável que indica se a FPGA está ligada/desligada.
-int power_state = 0;
-// variável para receber dados vindo da comunicação serial com a GUI
-int received;
-// variável auxiliar para ler bit a bit
-unsigned char bitread;
-// bool is_sent = false;
-// variável para verificar a conexão da FPGA com a GUI
-bool is_connected = false;
-// variável para bloquear o botão de ligar/desligar a FPGA.
-bool is_power_blocked = false;
+void UART_Init(unsigned int ubrr);
+void UART_Transmit(unsigned char data);
+unsigned char UART_Receive(void );
+void setup(void);
+int main(void );
 
-unsigned long beam_millis = clock(); // timestamp for last beam sent (testar função clock())
-// unsigned long sent_millis;            // ts for output clock state change
-unsigned long input_millis;     // ts for last input received
-unsigned long power_on_millis;  // ts for how long the board is on
-unsigned long power_off_millis; // ts for last power toggle
+/** Funções **/
+
+void UART_Init( unsigned int ubrr)
+{
+  /*Set baud rate */
+  UBRR0H = (unsigned char)(ubrr>>8);
+  UBRR0L = (unsigned char)ubrr;
+  /*Disable double speed*/
+  UCSR0A = 0;
+  /*Enable receiver and transmitter */
+  UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+  /* Set frame format: 8data, 1 stop bit, none parity */
+  UCSR0C = (0<<USBS0)|(1<<UCSZ01)|(1<<UCSZ00);
+}
+
+void UART_Transmit(unsigned char data)
+{
+  /* Wait for empty transmit buffer */
+  while (!(UCSR0A & (1<<UDRE0)))
+    ;
+  /* Put data into buffer, sends the data */
+  UDR0 = data;
+}
+
+unsigned char UART_Receive(void)
+{
+  /* Wait for data to be received */
+  while (!(UCSR0A & (1<<RXC0)))
+  ;
+  /* Get and return received data from buffer */
+  return UDR0;
+}
 
 void setup(void){
   DDRD = 0b11111100; //configura os pinos 2 a 7 como saída
   DDRB = 0b00000011; //configura os pinos 8 a 9 como saída
   PORTD = 0b00001111; //desativa os pinos 4 a 7 (zera a tensao)
   PORTB = 0b11111100; //desativa os pinos 8 a 9 (zera a tensao)
+  
+  UART_Init(MYUBRR);
 }
 
 int main() {
+  /** Inicialização das variáveis **/
+  
+  int power_state = 0; // variável que indica se a FPGA está ligada/desligada.
+  int received; // variável para receber dados vindo da comunicação serial com a GUI
+  unsigned char bitread; // variável auxiliar para ler bit a bit
+  bool is_connected = false; // variável para verificar a conexão da FPGA com a GUI
+  bool is_power_blocked = false; // variável para bloquear o botão de ligar/desligar a FPGA.
+  unsigned long beam_millis = clock(); // timestamp for last beam sent (testar função clock())
+  unsigned long input_millis;     // ts for last input received
+  unsigned long power_on_millis;  // ts for how long the board is on
+  unsigned long power_off_millis; // ts for last power toggle
+  
   setup();
-  // Loop principal para comunicação e controle da placa DE2-115
+  
+  /** Loop principal para comunicação e controle da placa DE2-115**/
   while(1){
     // Loop cuja condição verifica se há dados para leitura na porta serial
-
-      // Send beacon every 1 second
-
-      // Unblock fpga power in 10 sec after last toggle
+    while ((UCSR0A & (1<<RXC0)) == 0) {
+      if ((clock() - beam_millis) > 1000) {
+        beam_millis = clock();
+        if (is_connected)
+          UART_Transmit(50);
+        else
+          Serial.write(40);
+    }
+      
+    // Unblock fpga power in 10 sec after last toggle
+    if (is_power_blocked && (clock() - power_off_millis) > 10000)
+      is_power_blocked = false;
 
       // Disconnect and turn off fpga if user is idle for 10 min
 
       // Turn fpga off after 10 min of use
 
     input_millis = clock();
-    //received = Serial.read(); (buscar equivalente)
+    received = UART_Receive();
 
     // Confirm connection
     
